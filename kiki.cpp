897707a0dbc30c8b73cba86d93d68283f810e1a1
AlR0507 <160520283+AlR0507@users.noreply.github.com>
2024-11-30 23:14:14 -0800

    Add files via upload

---
+++ kiki.cpp
@@ -0,0 +1,19 @@
+#define NOMINMAX
+#include "Logica.h"
+#include "SFML.h"
+
+void static procesarCarpeta(const std::wstring& ruta) {
+    ListaEnlazada listaHashes;
+    wcout << L"Procesando la carpeta: " << ruta << L"...\n";
+    listar_y_hashear(ruta, listaHashes);
+    listaHashes.mostrar();
+    listaHashes.mostrarDuplicados(); 
+    listaHashes.eliminarDuplicados(); 
+}
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    Ventana ventana(procesarCarpeta);
+    ventana.run();
+    return 0;
+}
